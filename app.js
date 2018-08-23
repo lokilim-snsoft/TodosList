9506740275acf90f03888cc64374aa432183cd3d
LIM WAI HOONG <whlim@snsoft.my>
2018-08-23 16:29:32 +0800

    no message

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ todoController(app);
 
 //listen to port 3000
 app.listen(process.env.PORT || 3000);
-console.log('You are listening to port 3000');
+console.log('You are listening to port ' + process.env.PORT || 3000);
